71d70d1d381c3bc0d60f8da263df9fc1bd20f9de
Hamid Siddiqui <hamidsiddiqui1222@gmail.com>
2022-06-05 20:46:02 +0530

    Management system first commit

--- MANAGEMENT_SYSTEM.c
+++ MANAGEMENT_SYSTEM.c
@@ -5,18 +5,3 @@
 // ERP - library List of books category(Science, Maths ,etc.), Book code, DO-ISSUE/RETURN, PENALTY.
 
 // ERP - accounts Student name, DO-fee_Submission, Penalty, Mode of transaction(paytm, bank, cheque - with student name and batch and more (to just store the data only) ).
-
-#include <stdio.h>
-#include <conio.h>
-
-struct ERP
-{
-    
-};
-
-//  hello
-int main() {
-
-int i;
-
-}
