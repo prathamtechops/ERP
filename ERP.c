39521d7a6df49bbc8340eb969e7aa4f460858585
Hamid Siddiqui <hamidsiddiqui1222@gmail.com>
2022-06-05 19:56:32 +0530

    ERP FIRST COMMIT

---
+++ ERP.c
@@ -0,0 +1,22 @@
+// ERP - Edit-section (Student name, DOB, year, batch, mail-id/clg_mail-id, enroll-no. ), My Profile, Fee pending details, Attendance details.
+
+// ERP - Edit-section ( Faculty name, DOB, DOJ, Subject-teacher, mail-id/clg_mail-id, ID-no. ), My Profile.
+
+// ERP - library List of books category(Science, Maths ,etc.), Book code, DO-ISSUE/RETURN, PENALTY.
+
+// ERP - accounts Student name, DO-fee_Submission, Penalty, Mode of transaction(paytm, bank, cheque - with student name and batch and more (to just store the data only) ).
+
+#include <stdio.h>
+#include <conio.h>
+
+struct ERP
+{
+    
+};
+
+
+int main() {
+
+
+
+}
