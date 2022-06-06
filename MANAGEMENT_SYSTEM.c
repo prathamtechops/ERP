1be1cea980fa09083d092b9d421f650d6959225f
Hamid Siddiqui <hamidsiddiqui1222@gmail.com>
2022-06-06 21:31:03 +0530

    trial

--- MANAGEMENT_SYSTEM.c
+++ MANAGEMENT_SYSTEM.c
@@ -5,3 +5,57 @@
 // ERP - library List of books category(Science, Maths ,etc.), Book code, DO-ISSUE/RETURN, PENALTY.
 
 // ERP - accounts Student name, DO-fee_Submission, Penalty, Mode of transaction(paytm, bank, cheque - with student name and batch and more (to just store the data only) ).
+
+
+#include <stdio.h>
+#include <conio.h>
+
+struct MANAGEMENT_SYSTEM
+{
+        struct Student
+        {
+            char studentName[30];
+            char batchName[5];
+            char mailId[30];
+            char official_MailId[30];
+            char enrollNo;
+            int startYear;
+            int endYear;
+
+            struct Attendance
+            {
+                struct Sub
+                {
+                    int totalClasses;
+                    int attendendClasses;
+                    int totalPercentage;
+                };
+                int totalclasses;
+                int attendendclasses;
+                int totalpercentage;
+            };
+            
+            struct DOB
+            {
+                int Day;
+                int Month;
+                int Year;
+            };
+
+            struct Fee
+            {
+                int submitFee;
+                int pendingFee;
+            };
+            
+        };
+        
+};
+
+
+int main()
+{
+
+    // int i;
+
+}
