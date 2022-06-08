5508d261aa607ecf42b2a93f4cc0dba143e5c8d5
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-06-08 12:56:06 +0530

    Update MANAGEMENT_SYSTEM.c

--- MANAGEMENT_SYSTEM.c
+++ MANAGEMENT_SYSTEM.c
@@ -6,7 +6,6 @@
 
 // ERP - accounts Student name, DO-fee_Submission, Penalty, Mode of transaction(paytm, bank, cheque - with student name and batch and more (to just store the data only) ).
 
-
 #include <stdio.h>
 #include <conio.h>
 
@@ -17,45 +16,44 @@ void intro()
 
 struct MANAGEMENT_SYSTEM
 {
-        struct Student
+    struct Student
+    {
+        char studentName[30];
+        char batchName[5];
+        char mailId[30];
+        char official_MailId[30];
+        char enrollNo;
+        int startYear;
+        int endYear;
+
+        struct Attendance
         {
-            char studentName[30];
-            char batchName[5];
-            char mailId[30];
-            char official_MailId[30];
-            char enrollNo;
-            int startYear;
-            int endYear;
-
-            struct Attendance
+            struct Sub
             {
-                struct Sub
-                {
-                    int totalClasses;
-                    int attendendClasses;
-                    int totalPercentage;
-                };
-                int totalclasses;
-                int attendendclasses;
-                int totalpercentage;
+                int totalClasses;
+                int attendendClasses;
+                int totalPercentage;
             };
-            
-            struct DOB
-            {
-                int Day;
-                int Month;
-                int Year;
-            };
-
-            struct Fee
-            {
-                int submitFee;
-                int pendingFee;
-            };
-            
+            int totalclasses;
+            int attendendclasses;
+            int totalpercentage;
         };
-        
-        struct Library
+
+        struct DOB
+        {
+            int Day;
+            int Month;
+            int Year;
+        };
+
+        struct Fee
+        {
+            int submitFee;
+            int pendingFee;
+        };
+    };
+
+    struct Library
     {
         char student_name[256];
         char course_name[256];
@@ -65,13 +63,10 @@ struct MANAGEMENT_SYSTEM
         int book_id;
         int due_date = 0;
     };
-        
 };
-
 
 int main()
 {
 
     // int i;
-
 }
