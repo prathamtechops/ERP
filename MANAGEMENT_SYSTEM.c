acab35deb9fa48099e75450920b517f229c28b33
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-06-30 21:19:08 +0530

    C

--- MANAGEMENT_SYSTEM.c
+++ MANAGEMENT_SYSTEM.c
@@ -7,55 +7,14 @@
 // ERP - accounts Student name, DO-fee_Submission, Penalty, Mode of transaction(paytm, bank, cheque - with student name and batch and more (to just store the data only) ).
 
 #include <stdio.h>
+#include <stdlib.h>
+
 // #include <conio.h>
 
 void intro()
 {
     printf("WELCOME");
 }
-
-struct Student
-{
-    char studentName[30];
-    char batchName[5];
-    char mailId[30];
-    char official_MailId[30];
-    char enrollNo;
-    int startYear;
-    int endYear;
-
-    struct Attendance
-    {
-        struct Sub
-        {
-            int totalClasses;
-            int attendendClasses;
-            int totalPercentage;
-        };
-        int totalclasses;
-        int attendendclasses;
-        int totalpercentage;
-    };
-
-    struct DOB
-    {
-        int Day;
-        int Month;
-        int Year;
-    };
-
-    struct Fee
-    {
-        char student_name[256];
-        char course_name[256];
-        int roll_no;
-        char authorName[256];
-        char bank_name[256];
-        int book_id;
-        int submitFee;
-        int pendingFee;
-    };
-};
 
 struct Library
 {
@@ -81,6 +40,7 @@ int main()
     int no = 0;
     struct Library lib[no];
 
+    FILE *fp = fopen("test.txt", "a+");
     int a;
 
     printf("What do you want to do\n");
@@ -94,40 +54,60 @@ int main()
     switch (a)
     {
     case 1:
+
+        if (fp == NULL)
+        {
+            printf("File does not exist\n");
+            exit(1);
+        }
+
         printf("How many student have to issue a book\n");
         scanf("%d", &no);
         for (int i = 0; i < no; i++)
         {
+            fprintf(fp, "Student %d\n", i + 1);
             printf("Enter student name: ");
             scanf("%s", &lib[i].student_name);
+            fprintf(fp, "The name of the student is: %s \n", lib[i].student_name);
             printf("CourseName: ");
             scanf("%s", &lib[i].course_name);
-            printf("Book Name: ");
-            scanf("%s", &lib[i].book_name);
-            printf("Book ID: ");
-            scanf("%d", &lib[i].book_id);
-            printf("Roll_no: ");
-            scanf("%d", &lib[i].roll_no);
+            fprintf(fp, "Course name: %s\n", lib[i].course_name);
+
+            // Working till here after this progam stops
+
+            // printf("Book Name: ");
+            // scanf("%s", &lib[i].book_name);
+            // fprintf(fp, "Book Name: %s\n", lib[i].book_name);
+            // printf("Book ID: ");
+            // scanf("%d", &lib[i].book_id);
+            // fprintf(fp, "Book id: %d\n", lib[i].book_id);
+            // printf("Roll_no: ");
+            // scanf("%d", &lib[i].roll_no);
+            // fprintf(fp, "Roll_No: %d\n", lib[i].roll_no);
             printf("\n----------------------------------\n");
             printf("Entry recorded please return the book in 7 days.\n");
         }
+        fclose(fp);
         break;
     case 2:
-        if (no == 0)
-            printf("No record present");
-        else
-        {
-            printf("--------------------------------------------------------------------------------------------------------\n");
 
-            for (int i = 0; i < no; i++)
-            {
-                printf("\nStudent name: %s ", lib[i].student_name);
-                printf("\nCourseName: %s ", lib[i].course_name);
-                printf("\nRoll_no: %d", lib[i].roll_no);
-                printf("\nBook Name: %s", lib[i].book_name);
-                printf("\nBook ID: %d", lib[i].book_id);
-            }
+        printf("--------------------------------------------------------------------------------------------------------\n");
+
+        // for (int i = 0; i < no; i++)
+        // {
+        //     printf("\nStudent name: %s ", lib[i].student_name);
+        //     printf("\nCourseName: %s ", lib[i].course_name);
+        //     printf("\nRoll_no: %d", lib[i].roll_no);
+        //     printf("\nBook Name: %s", lib[i].book_name);
+        //     printf("\nBook ID: %d", lib[i].book_id);
+        // }
+        char c = fgetc(fp);
+        while (c != EOF)
+        {
+            printf("%c", c);
+            c = fgetc(fp);
         }
+
         break;
     case 3:
         listofbooks();
